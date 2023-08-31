52ca29a84a00daeb4bcfe4e8a17a4ae19a2b4913
Miguel Saliba <miguosaliba@gmail.com>
2023-08-31 15:57:02 -0400

    before move to kickstart.nvim

--- private_dot_config/nvim/lua/custom/executable_init.lua
+++ private_dot_config/nvim/lua/custom/executable_init.lua
@@ -1,2 +1,17 @@
 vim.opt.colorcolumn = "80"
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+
+
+local group = vim.api.nvim_create_augroup("CustomAutocmds", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost",
+  {
+    pattern = "/home/miguo/.local/share/chezmoi/*",
+    -- command = "!chezmoi apply --source-path %",
+    -- command = "echo 'hello'",
+    callback = function ()
+      vim.cmd("silent exe \"!chezmoi apply --source-path %\"")
+    end,
+    group = group
+  })
+
