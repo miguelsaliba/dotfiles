705198373aa960c4167d9e29aa66894315bf7f52
Miguel Saliba <36637899+miguelsaliba@users.noreply.github.com>
2024-02-09 01:27:23 -0500

    stuff

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -285,8 +285,8 @@ vim.keymap.set('n', '<leader>ts', ':Telescope symbols <CR>')
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
--- vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
--- vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Esc to remove search highlights
 vim.keymap.set('n', '<Esc>', ':noh <CR>')
