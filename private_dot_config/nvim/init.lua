231073a9d1bfec3bd84186cdd6f8f0c58bf62d07
Miguel Saliba <36637899+miguelsaliba@users.noreply.github.com>
2024-02-21 16:15:13 -0500

    updated ideavimrc

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -525,7 +525,12 @@ require('which-key').register {
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  clangd = {},
+  clangd = {
+    cmd = {
+      "clangd",
+      "--clang-tidy",
+    },
+  },
   -- gopls = {},
   pyright = {},
   rust_analyzer = {},
@@ -570,6 +575,7 @@ mason_lspconfig.setup_handlers {
       on_attach = on_attach,
       settings = servers[server_name],
       filetypes = (servers[server_name] or {}).filetypes,
+      cmd = (servers[server_name] or {}).cmd,
     }
   end
 }
