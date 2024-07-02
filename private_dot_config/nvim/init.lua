2eda08cc17a7a9477aa5e30e50e52ff936e2c93d
Miguel Saliba <36637899+miguelsaliba@users.noreply.github.com>
2024-07-03 02:45:28 +0300

    Added gitsigns keybinds

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -262,6 +262,45 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
     },
+    config = function(_, opts) 
+      local gitsigns = require('gitsigns')
+      gitsigns.setup(opts)
+  
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+  
+      -- Navigation
+      map('n', ']c', function()
+        if vim.wo.diff then
+          vim.cmd.normal({']c', bang = true})
+        else
+          gitsigns.nav_hunk('next')
+        end
+      end)
+  
+      map('n', '[c', function()
+        if vim.wo.diff then
+          vim.cmd.normal({'[c', bang = true})
+        else
+          gitsigns.nav_hunk('prev')
+        end
+      end)
+  
+      -- Actions
+      map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Stage hunk' })
+      map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Reset hunk' })
+      map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+      map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+      map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Stage buffer' })
+      map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'Unstage hunk' })
+      map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Reset buffer' })
+      map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'Hunk preview' })
+      map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end, { desc = 'Hunk blame' })
+      map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'Toggle line blame' })
+    end
   },
 
   -- NOTE: Plugins can also be configured to run lua code when they are loaded.
