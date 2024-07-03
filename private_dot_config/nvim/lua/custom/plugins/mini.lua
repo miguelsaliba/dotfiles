795498ee6906e1653f306829435f3aea8acc8568
Miguel Saliba <36637899+miguelsaliba@users.noreply.github.com>
2024-07-03 12:33:24 +0300

    Added mini move and files

--- private_dot_config/nvim/lua/custom/plugins/mini.lua
+++ private_dot_config/nvim/lua/custom/plugins/mini.lua
@@ -8,8 +8,9 @@ return {
       --  - va)  - [V]isually select [A]round [)]parenthen
       --  - yinq - [Y]ank [I]nside [N]ext [']quote
       --  - ci'  - [C]hange [I]nside [']quote
-      local spec_treesitter = require('mini.ai').gen_spec.treesitter
-      require('mini.ai').setup {
+      local miniai = require('mini.ai')
+      local spec_treesitter = miniai.gen_spec.treesitter
+      miniai.setup {
         n_lines = 1000,
         custom_textobjects = {
           F = spec_treesitter { a = '@function.outer', i = '@function.inner' },
@@ -21,20 +22,9 @@ return {
         },
       }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      -- require('mini.statusline').setup()
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
+      require('mini.move').setup()
+      require('mini.files').setup()
     end,
   },
 }
