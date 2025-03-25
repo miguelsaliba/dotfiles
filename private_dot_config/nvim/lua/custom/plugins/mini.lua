d93676b2236aa12476f4d409dada5cb96e6ce7e7
Miguel Saliba <36637899+miguelsaliba@users.noreply.github.com>
2025-03-25 19:03:22 -0400

    Updated to new nvim kickstart

--- private_dot_config/nvim/lua/custom/plugins/mini.lua
+++ private_dot_config/nvim/lua/custom/plugins/mini.lua
@@ -4,7 +4,7 @@ return {
     config = function()
       require('mini.surround').setup()
       require('mini.move').setup()
-      local minifiles = require('mini.files').setup()
+      require('mini.files').setup()
 
       local miniai = require('mini.ai')
       local spec_treesitter = miniai.gen_spec.treesitter
@@ -19,6 +19,22 @@ return {
           c = spec_treesitter { a = '@comment.outer', i = '@comment.inner' },
         },
       }
+
+      -- -- Simple and easy statusline.
+      -- --  You could remove this setup call if you don't like it,
+      -- --  and try some other statusline plugin
+      -- local statusline = require 'mini.statusline'
+      -- -- set use_icons to true if you have a Nerd Font
+      -- statusline.setup { use_icons = vim.g.have_nerd_font }
+      --
+      -- -- You can configure sections in the statusline by overriding their
+      -- -- default behavior. For example, here we set the section for
+      -- -- cursor location to LINE:COLUMN
+      -- ---@diagnostic disable-next-line: duplicate-set-field
+      -- statusline.section_location = function()
+      --   return '%2l:%-2v'
+      -- end
+
     end,
 
     vim.keymap.set({'n', 'v'}, '<leader>o', '<cmd>lua MiniFiles.open()<cr>', { desc = 'Mini Files' })
