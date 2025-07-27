d0a9af00d0c8eb0a479eaa5270a48e8d3d7b1e8c
Miguel Saliba <36637899+miguelsaliba@users.noreply.github.com>
2025-07-27 19:54:30 -0400

    cleanup

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -8,9 +8,6 @@ return {
 				cpp = { "clang-format" },
 				-- Conform can also run multiple formatters sequentially
 				python = { "isort", "black" },
-				--
-				-- You can use a sub-list to tell conform to run *until* a formatter
-				-- is found.
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				typescript = { "prettierd", "prettier", stop_after_first = true },
 				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -2,44 +2,42 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     lazy = false,
-    config = function()
-      require("lualine").setup({
-        options = {
-          component_separators = '|',
-          section_separators = '',
-          theme = 'auto',
-        },
-        sections = {
-          lualine_x = {
-            {
-              function ()
-                local reg = vim.fn.reg_recording()
-                if reg == "" then return "" end
-                return "Recording @" .. reg
-              end,
-            },
-            { 'selectioncount' },
-            { 'searchcount' },
+    opts = {
+      options = {
+        component_separators = '|',
+        section_separators = '',
+        theme = 'auto',
+      },
+      sections = {
+        lualine_x = {
+          {
+            function ()
+              local reg = vim.fn.reg_recording()
+              if reg == "" then return "" end
+              return "Recording @" .. reg
+            end,
           },
-          lualine_y = {
-            'encoding',
-            {
-              'filetype',
-              colored = true,
-              icon_only = true,
-              -- padding = { left = 1, right = 0, }, -- Removes the extra space on the right side
-            },
-            {
-              'lsp_status',
-              icons_enabled = false,
-            },
-          },
-          lualine_z = {
-            { 'progress' },
-            { 'location' },
-          }
+          { 'selectioncount' },
+          { 'searchcount' },
         },
-      })
-    end,
+        lualine_y = {
+          'encoding',
+          {
+            'filetype',
+            colored = true,
+            icon_only = true,
+            -- padding = { left = 1, right = 0, }, -- Removes the extra space on the right side
+          },
+          {
+            'lsp_status',
+            icons_enabled = false,
+          },
+        },
+        lualine_z = {
+          { 'progress' },
+          { 'location' },
+        }
+      },
+    },
   },
 }
