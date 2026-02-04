a3b04722bb3b073860f6f4607f62c574ccc115ac
Miguel Saliba <36637899+miguelsaliba@users.noreply.github.com>
2026-02-04 00:15:52 -0800

    fixed nvim-treesitter

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,21 +1,21 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		lazy = true, -- will be loading it using snacks.nvim
-		build = ":Copilot auth",
-		config = function()
-			require("copilot").setup({
-				panel = { enabled = false },
-				suggestion = {
-					enabled = false,
-					auto_trigger = false,
-				},
-				server = {
-					type = "binary",
-				},
-			})
-			-- vim.keymap.set('n', '<leader>co', vim.cmd('Copilot toggle'), { silent = true, desc = 'Toggle [co]pilot' })
-		end,
-	},
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    lazy = true, -- will be loading it using snacks.nvim
+    build = ':Copilot auth',
+    config = function()
+      require('copilot').setup({
+        panel = { enabled = false },
+        suggestion = {
+          enabled = false,
+          auto_trigger = false,
+        },
+        server = {
+          type = 'binary',
+        },
+      })
+      -- vim.keymap.set('n', '<leader>co', vim.cmd('Copilot toggle'), { silent = true, desc = 'Toggle [co]pilot' })
+    end,
+  },
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,6 +5,28 @@ return {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     build = ':TSUpdate',
+    config = function()
+      local ts = require('nvim-treesitter')
+      ts.install({
+        'lua',
+        'luadoc',
+        'vim',
+        'vimdoc',
+        'markdown',
+        'markdown_inline',
+        'go',
+        'hyprlang',
+        'html',
+        'rust',
+        'cpp'
+      })
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { '<filetype>' },
+        callback = function()
+          vim.treesitter.start()
+        end,
+      })
+    end,
   },
   {
     'stevearc/conform.nvim',
