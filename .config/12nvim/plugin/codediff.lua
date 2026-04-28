c0cdb214a8c2f869d3829e2b1c6a2e85c19069db
Miguel Saliba <36637899+miguelsaliba@users.noreply.github.com>
2026-04-27 22:01:07 -0400

    added git keymaps nvim

--- .config/12nvim/plugin/codediff.lua
+++ .config/12nvim/plugin/codediff.lua
@@ -1,3 +1,5 @@
 Util.later(function()
-    vim.pack.add({ 'https://github.com/esmuellert/codediff.nvim' })
+  vim.pack.add({ 'https://github.com/esmuellert/codediff.nvim' })
+
+  vim.keymap.set('n', '<leader>gd', '<cmd>CodeDiff<cr>', { desc = 'CodeDiff' })
 end)
--- .config/12nvim/plugin/fugitive.lua
+++ .config/12nvim/plugin/fugitive.lua
@@ -2,4 +2,7 @@ Util.later(function()
   vim.pack.add({
     'https://github.com/tpope/vim-fugitive',
   })
+
+  vim.keymap.set('n', '<leader>gg', '<cmd>Git<cr>', { desc = 'Fugitive' })
+  vim.keymap.set('n', '<leader>gs', '<cmd>Git status<cr>', { desc = 'git status' })
 end)
