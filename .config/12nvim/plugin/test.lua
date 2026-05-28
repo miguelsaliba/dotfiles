49c8442791c5380c2932d77604c9589072816c27
Miguel Saliba <36637899+miguelsaliba@users.noreply.github.com>
2026-05-27 23:20:06 -0700

    added yadm-git to nvim

--- .config/12nvim/plugin/test.lua
+++ .config/12nvim/plugin/test.lua
@@ -1,4 +1,5 @@
 -- Just a place to test plugins
 Util.later(function ()
+    do return end
     vim.pack.add({ 'https://github.com/dstein64/vim-startuptime' })
 end)
---
+++ .config/12nvim/plugin/yadm.lua
@@ -0,0 +1,4 @@
+Util.now(function()
+    vim.pack.add({'https://github.com/Kohei-Wada/yadm-git.nvim'})
+    require('yadm-git').setup()
+end)
